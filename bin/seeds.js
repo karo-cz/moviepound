6a2c57bd16b55c12d5b67d08a6c9becab666f6a1
Nora Nicklis <nora.nicklis@gmail.com>
2020-02-10 16:34:24 +0100

    update seed file

--- bin/seeds.js
+++ bin/seeds.js
@@ -20,11 +20,11 @@ Movie.collection.drop()
 for (movie of moviesJSON){
     Movie.create({title: movie.original_title, omdbId: movie.id})
     .then(movieDocument => {console.log("Movie created: ", movieDocument); 
-    axios.get(`https://api.themoviedb.org/3/movie/${movieDocument.omdbId}?api_key=`)
+    axios.get(`https://api.themoviedb.org/3/movie/${movieDocument.omdbId}?api_key=945edfeb81929f1fb50b3191c1073571`)
         .then(response => {console.log(response);
         let movieDetails = response.data; 
 
-        axios.get(`http://api.themoviedb.org/3/movie/${movieDocument.omdbId}/videos?api_key=`).then(
+        axios.get(`http://api.themoviedb.org/3/movie/${movieDocument.omdbId}/videos?api_key=945edfeb81929f1fb50b3191c1073571`).then(
             response => { 
                 console.log(response.data.results[0].key)
         Movie.updateOne({omdbId: movieDocument.omdbId }, {releaseDate: movieDetails.release_date, genre: movieDetails.genres, image: `https://image.tmdb.org/t/p/w1280${movieDetails.poster_path}`, trailer: `https://www.youtube.com/watch?v=${response.data.results[0].key}` }).then(response => console.log(response)).catch(err => console.log(err))
